87abd6fabba62016da2c8f2c22870446ffb32eff
devdanielcambinda <danielcambinda@gmail.com>
2021-03-22 15:58:53 +0000

    first commit

---
+++ Enunciado/euro2016.js
@@ -0,0 +1,121 @@
+/** 
+ * Aplicações multimédia - Trabalho Prático 1
+ * (c) Cláudio Barradas, 2021
+ * 
+ */
+
+const game = {}; // encapsula a informação de jogo. Está vazio mas vai-se preenchendo com definições adicionais.
+
+// sons do jogo
+const sounds = {
+	background: null,
+	flip: null,
+	success: null,
+	hide: null
+};
+
+// numero de linhas e colunas do tabuleiro;
+const ROWS = 6;
+const COLS = 8;
+
+game.sounds = sounds; // Adicionar os sons sons do jogo ao objeto game.
+game.board  = Array(COLS).fill().map(() => Array(ROWS)); // criação do tabuleiro como um array de 6 linhas x 8 colunas
+ 
+// Representa a imagem de uma carta de um país. Esta definição é apenas um modelo para outros objectos que sejam criados
+// com esta base através de let umaFace = Object.create(face).
+const face = {
+	country: -1,
+	x: -1,
+	y: -1
+};
+
+const CARDSIZE = 100; 	// tamanho da carta (altura e largura)
+let faces = []; 		// Array que armazena objectos face que contêm posicionamentos da imagem e códigos dos paises
+ 
+
+window.addEventListener("load", init, false);
+
+function init() {
+	game.stage = document.querySelector("#stage");
+	setupAudio(); 		// configurar o audio
+	getFaces(); 		// calcular as faces e guardar no array faces
+	createCountries();	// criar países
+	game.sounds.background.play();
+	 
+	//completar
+}
+
+// Cria os paises e coloca-os no tabuleiro de jogo(array board[][])
+function createCountries() {
+	/* DICA:
+	Seja umaCarta um elemento DIV, a imagem de carta pode ser obtida nos objetos armazenados no array faces[]; o verso da capa 
+	está armazenado na ultima posicao do array faces[]. Pode também ser obtido através do seletor de classe .escondida do CSS.
+		umaCarta.classList.add("carta"); 	
+		umaCarta.style.backgroundPositionX=faces[0].x;
+		umaCarta.style.backgroundPositionX=faces[0].y;
+
+		Colocar uma carta escondida:
+			umaCarta.classList.add("escondida");
+			
+		virar a carta:
+			umaCarta.classList.remove("escondida");
+    */
+
+ 
+}
+
+// Adicionar as cartas do tabuleiro à stage
+function render() {
+	 
+}
+
+
+// baralha as cartas no tabuleiro
+function scramble() {
+ 
+}
+
+
+
+
+/* ------------------------------------------------------------------------------------------------  
+ ** /!\ NÃO MODIFICAR ESTAS FUNÇÕES /!\
+-------------------------------------------------------------------------------------------------- */
+ 
+// configuração do audio
+function setupAudio() {
+	game.sounds.background = document.querySelector("#backgroundSnd");
+	game.sounds.success = document.querySelector("#successSnd");
+	game.sounds.flip = document.querySelector("#flipSnd");
+	game.sounds.hide = document.querySelector("#hideSnd");
+	game.sounds.win = document.querySelector("#goalSnd");
+
+	// definições de volume;
+	game.sounds.background.volume=0.05;  // o volume varia entre 0 e 1
+
+	// nesta pode-se mexer se for necessário acrescentar ou configurar mais sons
+
+}
+
+// calcula as coordenadas das imagens da selecao de cada país e atribui um código único
+function getFaces() {
+/* NÂO MOFIFICAR ESTA FUNCAO */
+	let offsetX = 1;
+	let offsetY = 1;
+	for (let i = 0; i < 5; i++) {
+		offsetX = 1;
+		for (let j = 0; j < 5; j++) {
+			let countryFace = Object.create(face); 				// criar um objeto com base no objeto face
+			countryFace.x = -(j * CARDSIZE + offsetX) + "px";   // calculo da coordenada x na imagem
+			countryFace.y = -(i * CARDSIZE + offsetY) + "px";   // calculo da coordenada y na imagem
+			countryFace.country = "" + i + "" + j; 			    // criação do código do país
+			faces.push(countryFace); 					        // guardar o objeto no array de faces
+			offsetX += 2;
+		}
+		offsetY += 2;
+	}
+}
+
+/* ------------------------------------------------------------------------------------------------  
+ ** /!\ NÃO MODIFICAR ESTAS FUNÇÕES /!\
+-------------------------------------------------------------------------------------------------- */
